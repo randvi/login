582e51954ff156e024f73767aeb26645c2dee53a
randvi <6535761@gmail.com>
2018-07-16 00:06:08 +0300

    Add login form

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer(function(request, response) {
-    //console.log("Hello pinMode!");
     console.log(request.method, request.url);
     if (request.url === '/style.css') {
         const css = fs.readFileSync('style.css','utf-8');
         response.end(css);
+    } else if (request.url === '/user.png') {
+        const user = fs.readFileSync('user.png');
+        response.end(user);
+    } else if (request.url === '/bg.jpg') {
+        const bg = fs.readFileSync('bg.jpg');
+        response.end(bg);  
     } else {
         const html = fs.readFileSync('index.html','utf-8');
         response.end(html);
